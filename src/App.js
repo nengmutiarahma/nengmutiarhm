53a9bab1c7da3a85689a9bfc5410602090b7689c
nengmutiarahma <mutiarahma042@gmail.com>
2023-11-15 00:53:08 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+function Catatan1(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div class="card p-2 mb-2 bg-info bg-gradient">
+        <div className="card-header d-flex justify-content-between">
+          <h5 class="card-title">w11vocasia</h5>
+          <div class="card-text fs-5">{props.dates}</div>
+        </div>
+        <p class="card-text fs-5 text-start">
+          vocasia keren 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
+  );
+}
+function Catatan2(props) {
+  return (
+    <>
+      <div class="card p-2 mb-2 bg-info bg-gradient">
+        <div className="card-header d-flex justify-content-between">
+          <h5 class="card-title">vocasia good</h5>
+          <div class="card-text fs-5">{props.dates}</div>
+        </div>
+        <p class="card-text fs-5 text-start">vocasia best</p>
+      </div>
+    </>
+  );
+}
+function Catatan3(props) {
+return (
+    <>
+      <div class="card p-2 mb-2 bg-info bg-gradient">
+        <div className="card-header d-flex justify-content-between">
+          <h5 class="card-title">vocasia good</h5>
+          <div class="card-text fs-5">{props.dates}</div>
+        </div>
+        <p class="card-text fs-5 text-start">
+          vocasia best
+        </p>
+      </div>
+    </>
+  );
+}
+function App() {
+  const name='n.mutiarahma';
+  return (
+    <div className="App card p-3">
+      <h1 class="text-center mb-3">Todo List {name}</h1>
+      
+      <div>
+        <Catatan1 dates="08/11/2023" />
+        <Catatan2 dates="09/11/2023" />
+        <Catatan3 dates="10/11/2023" />
+      </div>
+    </div>
   );
 }
 
